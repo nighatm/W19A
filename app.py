a654dc388a289740a4e4ca8654ee309441266c25
nighatm <67768488+nighatm@users.noreply.github.com>
2020-10-31 09:21:42 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 import addition
 import subtraction
 import multiplication
-import division
+import divide
 
-print("Choose an operation")
-print("1. Add")
-print ("2. Subtract")
-print("3. Multiply")
-print("4. Divide ")
+print("Please choose an operation")
+print("1: Addition")
+print("2: Subtraction")
+print("3: Multiplication")
+print("4: Division")
 
-userChoice = input ("Enter Choice 1 or 2 or 3 or 4  :  ")
-if userChoice in ('1', '2', '3', '4'  ):
-    number1 = float(input("Enter first number: "))
-    number2 = float(input ("Enter second number: "))
-    if userChoice == '1':
-        print(number1, "+", number2, "=", addition.add(number1, number2))
-    elif userChoice == '2':
-        print(subtraction.subtract(number1,number2))
-    elif userChoice == '3':
-        print (multiplication.multiply(number1,number2))
-    elif userChoice == '4':
-        print(division.divide)
+try:
+    userChoice = int(input ("Enter Choice 1 or 2 or 3 or 4  :  "))
+except:
+    print ('You have entered an invalid option.')
 else:
-    print("Invalid user input")
+     if userChoice in ('1', '2', '3', '4'  ):
+        number1 = int(input("Enter first number: "))
+        number2 = int(input ("Enter second number: "))
+        if (userChoice == '1'):
+            print( "Result is: " str(addition.add(number1, number2)))
+        elif (userChoice == '2'):
+            print("Result is: " str(subtraction.subtract(number1, number2)))
+        elif (userChoice == '3'):
+            print ("Result is: " str(multiplication.multiply(number1, number2)))
+        elif (userChoice == '4'):
+            print("Result is: " str(divide.division(number1,number2)))    
+    else:
+        print("Invalid user input")
